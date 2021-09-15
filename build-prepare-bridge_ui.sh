bdf1d91872cc278991db39c279551e464e313e90
SSen <i@ssen.name>
2021-09-15 21:14:59 +0900

    chore: remove useless .gitignore files after lib packaging

--- build-prepare-bridge_ui.sh
+++ build-prepare-bridge_ui.sh
@@ -3,3 +3,4 @@ DOCKER_BUILDKIT=1 docker build -f solana/Dockerfile.wasm -o type=local,dest=. so
 npm ci --prefix ethereum
 npm ci --prefix sdk/js
 npm run build --prefix sdk/js
+rm -f sdk/js/src/solana/**/.gitignore
