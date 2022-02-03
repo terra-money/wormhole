1a5647b93eecd7e230558e0269037442e0a3f036
suzzax <59423378+suzzax@users.noreply.github.com>
2022-02-03 10:01:18 +0800

    Update App.js

--- bridge_ui/src/App.js
+++ bridge_ui/src/App.js
@@ -1,5 +1,5 @@
-import { makeStyles } from "@material-ui/core";
-import HeaderText from "./components/HeaderText";
+import { makeStyles, Link, Container, Typography} from "@material-ui/core";
+import Header from "./images/Header.png";
 
 const useStyles = makeStyles((theme) => ({
   bg: {
@@ -7,13 +7,78 @@ const useStyles = makeStyles((theme) => ({
     flexDirection: "column",
     minHeight: "100vh",
   },
+  headerImage: {
+    position: "absolute",
+    zIndex: -1,
+    top: 0,
+    background: `url(${Header})`,
+    backgroundRepeat: "no-repeat",
+    backgroundPosition: "top -500px center",
+    backgroundSize: "2070px 1155px",
+    width: "100%",
+    height: 1155,
+  },
+  centeredContainer: {
+    marginTop: theme.spacing(14),
+    marginBottom: theme.spacing(26),
+    minHeight: 208,
+    textAlign: "center",
+    width: "100%",
+  },
 }));
 
 function App() {
   const classes = useStyles();
   return (
     <div className={classes.bg}>
-      <HeaderText white>Portal is Temporarily Unavailable</HeaderText>
+      <div className={classes.content}>
+        <div className={classes.headerImage} />
+           <Container maxWidth="md">
+             <div className={classes.centeredContainer} style={{marginBottom: 30}}>
+              <Typography
+                variant="h2"
+                component="h2"
+              >
+                We're actively working to get Portal back up and running.
+              </Typography>
+          </div>
+          <div className={classes.centeredContainer} style={{marginTop: 0}}>
+
+            <Typography
+              variant="h2"
+              component="h2"
+            >
+              A fix has been deployed and all funds are safe.
+            </Typography>
+        </div>
+        </Container>
+
+        <Container maxWidth="lg">
+          <div className={classes.centeredContainer} style={{marginBottom: 0}}>
+              <Typography
+                variant="h2"
+                component="h2"
+              >
+                Thank you for your support and trust.
+            </Typography>
+            </div>
+            <div className={classes.centeredContainer} style={{marginTop: 0}}>
+              <Typography
+                variant="h2"
+                component="h2"
+              >
+                For more updates, please follow us on&nbsp;
+                <Link
+                  href="https://twitter.com/wormholecrypto"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  color="inherit"
+                  className={classes.link}
+                >twitter</Link>.
+              </Typography>
+          </div>
+        </Container>
+       </div>
     </div>
   );
 }
